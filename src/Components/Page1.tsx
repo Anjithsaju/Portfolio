d24fd36324cd8db9dae5b92234b3159ee35e8d3d
Anjithsaju <anjithsaju123@gmail.com>
2024-07-26 02:40:43 +0530

    Initial commit

---
+++ src/Components/Page1.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Styles.css";
+function Page1() {
+  return (
+    <>
+      <div className="page1">
+        <img src="./src/Images/hi.gif" alt="nothing" className="imgname" />
+        <img src="./src/Images/fog.gif" alt="nothing" className="fog" />
+        {/* <button>1</button>
+        <button>1</button>
+        <button>1</button>
+        <button>1</button>
+        <button>1</button> */}
+      </div>
+    </>
+  );
+}
+
+export default Page1;
---
+++ src/Components/Page2.tsx
@@ -0,0 +1,18 @@
+function Page2() {
+  return (
+    <>
+      <div className="page2">
+        <img src="./src/Images/About.gif" alt="" className="about" />
+        <img src="./src/Images/brush.png" alt="" className="brush" />
+        <img src="./src/Images/profile.jpg" alt="" className="profile" />
+        <p className="intro">
+          "Hi, I'm Anjith Saju, a full stack developer proficient in JavaScript,
+          HTML, CSS, React, Python, Flask, Node.js, and Dash. I create dynamic
+          and responsive web applications, bridging the gap between design and
+          functionality."
+        </p>
+      </div>
+    </>
+  );
+}
+export default Page2;

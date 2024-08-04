5491bfdd0428b227677db278dec647d0506b9579
Anjithsaju <anjithsaju123@gmail.com>
2024-08-04 18:10:08 +0530

    ygyy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import React from "react";
-import Page1 from "./Components/Page1";
-import Page2 from "./Components/Page2";
-import Cards from "./Components/Cards";
 import Theone from "./Components/Theone";
-import Works from "./Components/Works";
-import Intro from "./Components/Intro";
+
 function App() {
   return (
     <>

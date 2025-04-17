846abbe37e4277a7ce6eaf6faaa48e6a01a93ad0
Anjith saju <anjithsaju123@gmail.com>
2025-04-17 23:39:54 +0530

    neww

--- src/Components/Experience.tsx
+++ src/Components/Experience.tsx
@@ -2,83 +2,94 @@ import "./Experience.css";
 import React, { useState } from "react";
 
 const Experience: React.FC = () => {
-  // State to track the active index
   const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
-  // Handler to set the active index and update class names
+  // Handler to set the active index
   const handleImageClick = (index: number) => {
-    setActiveIndex(index); // Update the active index
+    setActiveIndex(index);
   };
 
   return (
     <>
-    <div className="thi">
-    <h1>Padwan To Master</h1>
-    <section className="section1">
-  {/* First div */}
-  <div className={activeIndex === 0 ? "main" : "main down"}>
-    <img
-      src="https://i.postimg.cc/wMFjs7nC/childd.png"
-      className="hover-image"
-      alt="padwan"
-      style={{ width: "19vw" }} // Fixed style syntax
-      onClick={() => handleImageClick(0)} // Set active index to 0
-    />
-    <div>
-      <h4>Learning HTML, CSS, JavaScript</h4>
-      <p>
-        Last year, I started learning web development with HTML, CSS, and JavaScript. I worked on
-        self-paced projects to strengthen my skills and explored responsive design and basic animations.
-      </p>
-    </div>
-  </div>
+      <div className="thi">
+        <h1>Padwan To Master</h1>
+        <section className="section1">
+          {/* First div */}
+          <div className={activeIndex === 0 ? "main active" : "main"}>
+            <img
+              src="https://i.postimg.cc/wMFjs7nC/childd.png"
+              className="hover-image"
+              alt="padwan"
+              style={{ width: "19vw" }}
+              onClick={() => handleImageClick(0)} // Set active index to 0
+            />
+            <img className="ship" src="./src/Images/ship.png" alt="" />
+            <div>
+              
+              <h4>Learning HTML, CSS, JavaScript</h4>
+              <p>
+                Last year, I started learning web development with HTML, CSS, and
+                JavaScript. I worked on self-paced projects to strengthen my skills
+                and explored responsive design and basic animations.
+              </p>
+            </div>
+          </div>
 
-  {/* Second div */}
-  <div className={activeIndex === 1 ? "main" : "main down"}>
-    <img
-      src="https://i.postimg.cc/HnTLxQv0/knight.png"
-      alt="Knight"
-      className="hover-image2"
-      style={{ width: "24vw" ,position: "relative",
-        bottom: "0vw"}} // Fixed style syntax
-      onClick={() => handleImageClick(1)} // Set active index to 1
-    />
-    <div>
-      <h4>Cybersecurity Internship</h4>
-      <p>
-        I attended an internship in Chennai, exploring the world of cybersecurity. I learned about secure
-        coding, penetration testing, and how to protect web applications.
-      </p>
-    </div>
-  </div>
+          {/* Second div */}
+          <div className={activeIndex === 1 ? "main active" : "main"}>
+            <img
+              src="https://i.postimg.cc/HnTLxQv0/knight.png"
+              alt="Knight"
+              className="hover-image2"
+              style={{
+                width: "24vw",
+                position: "relative",
+                bottom: "0vw",
+              }}
+              onClick={() => handleImageClick(1)} // Set active index to 1
+            />
+            <div>
+              <h4>Cybersecurity Internship</h4>
+              <p>
+                I attended an internship in Chennai, exploring the world of
+                cybersecurity. I learned about secure coding, penetration testing, and
+                how to protect web applications.
+              </p>
+            </div>
+          </div>
 
-  {/* Third div */}
-  <div className={activeIndex === 2 ? "main" : "main down"}>
-    <img
-      src="https://i.postimg.cc/0QYQPtn2/mas.png"
-      className="hover-image3"
-      alt="padwan"
-      style={{ width: "34vw",position: "relative",
-        bottom: "-3.5vw" }} // Fixed style syntax
-      onClick={() => handleImageClick(2)} // Set active index to 2
-    />
-    <div>
-      <h4>Assisting the Blind</h4>
-      <p>
-        This year, I am working on a hardware-software project designed to assist blind individuals. Using IoT
-        and sensors, we aim to improve accessibility for visually impaired users.
-      </p>
-    </div>
-  </div>
-</section>
+          {/* Third div */}
+          <div className={activeIndex === 2 ? "main active" : "main"}>
+            <img
+              src="https://i.postimg.cc/0QYQPtn2/mas.png"
+              className="hover-image3"
+              alt="padwan"
+              style={{
+                width: "34vw",
+                position: "relative",
+                bottom: "-3.5vw",
+              }}
+              onClick={() => handleImageClick(2)} // Set active index to 2
+            />
+            <div>
+              <h4>Assisting the Blind</h4>
+              <p>
+                This year, I am working on a hardware-software project designed to
+                assist blind individuals. Using IoT and sensors, we aim to improve
+                accessibility for visually impaired users.
+              </p>
+            </div>
+          </div>
+        </section>
 
-      <img className="saber" src="https://i.postimg.cc/yYm5MGLf/lightsaber.png" alt="" />
+        <img
+          className="saber"
+          src="https://i.postimg.cc/yYm5MGLf/lightsaber.png"
+          alt=""
+        />
       </div>
     </>
   );
 };
 
 export default Experience;
-
-
-
--- src/Components/Works.tsx
+++ src/Components/Works.tsx
@@ -1,7 +1,5 @@
 import "./Works.css";
-// interface Props {
-//   workclass: string; // Add the pid prop to the Props interface
-// }
+
 function Works() {
   return (
     <>
@@ -10,12 +8,12 @@ function Works() {
           <img
             src="https://i.postimg.cc/bJwgTqd0/split.png"
             className="card-img-top"
-            alt=""
+            alt="Split-App"
           />
           <div className="card-body">
             <h5 className="card-title">Split-App</h5>
             <p className="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Split group expenses and track balances with ease.
             </p>
             <a
               href="https://anjithsaju.github.io/"
@@ -26,19 +24,20 @@ function Works() {
             </a>
           </div>
         </div>
+
         <div id="crd" className="card">
           <img
-            src="https://i.postimg.cc/LsNBhGvJ/expense.png"
+            src="https://i.postimg.cc/t4x05cgx/Screenshot-2025-04-17-232540.png"
             className="card-img-top"
-            alt=""
+            alt="Baaki Pinne Tharam"
           />
           <div className="card-body">
-            <h5 className="card-title">Expense Tracker</h5>
+            <h5 className="card-title">Baaki Pinne Tharam</h5>
             <p className="card-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Curious??....Check out the site to know more.
             </p>
             <a
-              href="https://anjithsaju.github.io/Expenses/"
+              href="https://baakipinnetharam.vercel.app/"
               target="_blank"
               className="btn btn-danger"
             >
@@ -46,17 +45,17 @@ function Works() {
             </a>
           </div>
         </div>
+
         <div id="crd" className="card">
           <img
             src="https://i.postimg.cc/D0bqj0hG/sign.png"
             className="card-img-top"
-            alt=""
+            alt="Talents"
           />
           <div className="card-body">
-            <h5 className="card-title">Sign in</h5>
+            <h5 className="card-title">Talents</h5>
             <p className="card-text">
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              A platform to post and discover unique talents.
             </p>
             <a
               href="https://anjithsaju.github.io/sighinAS.github.io/"
@@ -67,37 +66,30 @@ function Works() {
             </a>
           </div>
         </div>
+
         <div id="crd" className="card">
           <img
-            src="https://i.postimg.cc/3RqXFps7/link.png"
+            src="https://i.postimg.cc/8CR28rMk/Screenshot-2025-04-17-232610.png"
             className="card-img-top"
-            alt=""
+            alt="THEIA"
           />
           <div className="card-body">
-            <h5 className="card-title">Linktree</h5>
+            <h5 className="card-title">THEIA</h5>
             <p className="card-text">
-              {" "}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              AI-powered device to help the visually impaired.
             </p>
             <a
-              href="https://anjithsaju.github.io/Linktree/"
+              href="https://theia-pied.vercel.app/"
               target="_blank"
               className="btn btn-danger"
             >
               Visit Site
             </a>
-            {/* <a
-                href="https://anjithsaju.github.io/Linktree/"
-                target="_blank"
-                className="btn btn-info"
-                style={{ marginLeft: "1.1vw" }}
-                >
-                View Code
-                </a> */}
           </div>
         </div>
       </div>
     </>
   );
 }
+
 export default Works;
